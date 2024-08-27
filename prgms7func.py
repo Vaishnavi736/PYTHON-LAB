87341bb64ac3bbc795e3983c01e9d70678f3386c
Vaishnavi736 <161019478+Vaishnavi736@users.noreply.github.com>
2024-08-27 10:22:17 +0530

    Create prgms7func.py

---
+++ prgms7func.py
@@ -0,0 +1,12 @@
+#FUNCTION CREATION
+def func():
+    print("Hello")
+func()
+def func1(a,b):
+    print(a+b)
+func1(6,4)
+def func2(*tab):
+     print(tab)
+     for i in tab:
+          print(i)        
+func2("oh","my","god",1,2,3)          
