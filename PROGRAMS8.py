9ec1d58de2bc63ef1a7c337aed494154e86853f1
Vaishnavi736 <161019478+Vaishnavi736@users.noreply.github.com>
2024-09-17 12:11:52 +0530

    Update PROGRAMS8.py

--- PROGRAMS8.py
+++ PROGRAMS8.py
@@ -17,7 +17,28 @@ if a<=0:
      print("Enter a positive integer")
 else:
      print("Fibonacci sequence: ")
-     for i in range(a):
-          print(rfib(i))     
-          
-      
+     print(0)
+     for i in range(a-1):
+          print(rfib(i))
+#GCD
+# Python code to demonstrate naive
+# method to compute gcd ( Loops )
+
+def computeGCD(x, y):
+
+	if x > y:
+		small = y
+	else:
+		small = x
+	for i in range(1, small + 1):
+		if((x % i == 0) and (y % i == 0)):
+			gcd = i
+			
+	return gcd
+
+a = 60
+b = 48
+
+# prints 12
+print ("The gcd of 60 and 48 is : ", end="")
+print (computeGCD(60,48))
