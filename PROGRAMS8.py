f9e423ab09ffb6a20a2e62aef0c426b3ab4d8e87
Vaishnavi736 <161019478+Vaishnavi736@users.noreply.github.com>
2024-09-24 08:33:58 +0530

    Update PROGRAMS8.py

--- PROGRAMS8.py
+++ PROGRAMS8.py
@@ -28,4 +28,36 @@ def gcd(a,b):
 a=int(input("Enter a"))
 b=int(input("Enter b"))
 print("GCD is ",gcd(a,b))
+#KEY EXISTS OR NOT
+mydict={1:"class",2:"crew",3:72,4:"lab"}
+key=int(input("Enter key"))
+if key in mydict:
+        print("Key exists")
+else:
+      print("Key does not exists")
+#ADD KEY VALUE PAIR
+mydict={1:"class",2:"crew",3:72,4:"lab"}
+mydict.update({5:"cse"})
+print(mydict)
+#SUM OF ITEMS IN A DICT
+mydict={1:"class",2:"crew",3:"python",4:"lab"}
+sum=" "
+for i in mydict.values():
+    sum=sum+i+" "
+print("sum of all items ",sum)    
+#second example
+mydict={1:84,2:34,3:22,4:21}
+sum=0
+for i in mydict.values():
+    sum=sum+i
+print("sum of all items ",sum)    
+#161
+#LAMBDA FUNCTION FOR ODD AND EVEN
+v=lambda n:n%2
+x=int(input("Enter number"))
+if v(x)==0:
+    print("Even")
+else:
+    print("Odd")
+    
         
